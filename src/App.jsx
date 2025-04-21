eeef472cd45f93b0e8653c8a37fec42ac3787161
Raksivaa <kazulaw69@gmail.com>
2025-04-21 18:02:10 +0545

    Added About section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Contact from "./components/Contact";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
       <Contact />
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ function Navbar() {
               <a href="/">Home</a>
             </li>
             <li>
-              <a href="#">About</a>
+              <a href="#about">About</a>
             </li>
             <li>
               <a href="#">Skills</a>
