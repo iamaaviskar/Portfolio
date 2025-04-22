420bc511551dbf406e7ecb4c4a5bf88df804803b
Raksivaa <kazulaw69@gmail.com>
2025-04-22 15:26:05 +0545

    Removed about component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Contact from "./components/Contact";
-import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <About />
+
       <Contact />
     </>
   );
